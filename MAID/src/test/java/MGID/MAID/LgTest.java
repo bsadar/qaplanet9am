7933a964a3425866f072af980ea719d137952eb0
qaplanet <qaplanet@qaplanet-PC>
2019-09-25 10:35:22 +0530

    updating for version1

---
+++ MAID/src/test/java/MGID/MAID/LgTest.java
@@ -0,0 +1,20 @@
+package MGID.MAID;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class LgTest {
+  @Test
+  public void f() {
+	  System.setProperty("webdriver.gecko.driver", "D:\\SelWD\\Drvers\\geckodriver.exe");
+		WebDriver bo=new FirefoxDriver();
+		bo.manage().window().maximize();
+		
+	    bo.get("http://apps.qaplanet.in/qahrm/login.php");
+	   // bo.getTitle();
+		System.out.println( bo.getTitle());
+		
+		bo.close();
+  }
+}
